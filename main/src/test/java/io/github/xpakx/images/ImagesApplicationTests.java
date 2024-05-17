3452e853c193ea7a34bdff2be998f0fd1a9c871c
xpakx <55586470+xpakx@users.noreply.github.com>
2024-05-17 21:32:32 +0200

    chore(main): add main backend service

---
+++ main/src/test/java/io/github/xpakx/images/ImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
