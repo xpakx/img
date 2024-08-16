e69a5a2212d7c8bcafb1faa8b13f43dd25dbec2f
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-16 14:45:17 +0200

    chore(main): configure UserDetailsService

--- main/src/main/java/io/github/xpakx/images/account/UserRole.java
+++ main/src/main/java/io/github/xpakx/images/account/UserRole.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserRole {
+public class UserRole implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
---
+++ main/src/main/java/io/github/xpakx/images/account/UserService.java
@@ -0,0 +1,25 @@
+package io.github.xpakx.images.account;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var userAccount = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No user with username " + username));
+        return new org.springframework.security.core.userdetails.User(userAccount.getUsername(), userAccount.getPassword(), userAccount.getRoles());
+    }
+
+    public UserDetails userAccountToUserDetails(io.github.xpakx.images.account.User userAccount) {
+        return new User(userAccount.getUsername(), userAccount.getPassword(), userAccount.getRoles());
+    }
+}
