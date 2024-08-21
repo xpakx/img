7324f29f221a6dc2deb875a1dad68d664278d3da
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-21 14:22:01 +0200

    test(frontend): fix new tests

--- frontend/src/app/image/upload.service.spec.ts
+++ frontend/src/app/image/upload.service.spec.ts
@@ -1,12 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { UploadService } from './upload.service';
 
 describe('UploadService', () => {
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
   let service: UploadService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(UploadService);
   });
 
--- frontend/src/app/image/upload/upload.component.spec.ts
+++ frontend/src/app/image/upload/upload.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { UploadService } from '../upload.service';
 
 import { UploadComponent } from './upload.component';
 
 describe('UploadComponent', () => {
+  let uploadService: jasmine.SpyObj<UploadService>;
   let component: UploadComponent;
   let fixture: ComponentFixture<UploadComponent>;
 
   beforeEach(async () => {
+    uploadService = jasmine.createSpyObj('UploadService', ['sendSound']);
     await TestBed.configureTestingModule({
-      declarations: [ UploadComponent ]
+      declarations: [ UploadComponent ],
+      providers: [
+        { provide: UploadService, useValue: uploadService },
+        FormBuilder
+      ]
     })
     .compileComponents();
 
