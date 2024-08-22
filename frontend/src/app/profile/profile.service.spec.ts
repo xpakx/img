f72dc7be38d80d2b9b747d0cb055eab8a995a106
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-22 14:37:55 +0200

    test(frontend): fix default test for profile service

--- frontend/src/app/profile/profile.service.spec.ts
+++ frontend/src/app/profile/profile.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProfileService } from './profile.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('ProfileService', () => {
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
   let service: ProfileService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+    imports: [],
+    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+});
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(ProfileService);
   });
 
