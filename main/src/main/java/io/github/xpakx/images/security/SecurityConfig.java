4f89d060b49a43d10499c5839bd7a75570210fdf
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-23 12:45:18 +0200

    chore(main): add delete to allowed methods in cors config

--- main/src/main/java/io/github/xpakx/images/security/SecurityConfig.java
+++ main/src/main/java/io/github/xpakx/images/security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "DELETE"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
