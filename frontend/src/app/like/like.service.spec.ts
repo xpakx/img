ec3590d591581a08acb420bbd35e86d6a60a345a
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-23 13:05:42 +0200

    chore(frontend): fix tests that should use http test client

--- frontend/src/app/like/like.service.spec.ts
+++ frontend/src/app/like/like.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LikeService } from './like.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('LikeService', () => {
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
   let service: LikeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+    imports: [],
+    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+});
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(LikeService);
   });
 
