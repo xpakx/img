cf12a99c8c8284c29e9d8747541433bf8377825e
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-25 12:32:31 +0200

    chore(main): add entity for profile

---
+++ main/src/main/java/io/github/xpakx/images/profile/ProfileRepository.java
@@ -0,0 +1,6 @@
+package io.github.xpakx.images.profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+}
