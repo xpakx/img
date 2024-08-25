496b5630d69078e53351ccd5144b123d1d9beaa4
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-25 12:02:42 +0200

    test(frontend): fix default test for follow service

--- frontend/src/app/follow/follow.service.spec.ts
+++ frontend/src/app/follow/follow.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FollowService } from './follow.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('FollowService', () => {
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
   let service: FollowService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+    });
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(FollowService);
   });
 
