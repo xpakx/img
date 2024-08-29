4fdfcc83f01a9ace2200d9a482185b77db30baf5
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-29 12:12:40 +0200

    chore(main): add entity for private messages

---
+++ main/src/main/java/io/github/xpakx/images/priv/PrivateMessage.java
@@ -0,0 +1,46 @@
+package io.github.xpakx.images.priv;
+
+import io.github.xpakx.images.account.User;
+import io.github.xpakx.images.image.Image;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PrivateMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    private String content;
+
+    @Column(name = "is_read")
+    private boolean read;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_Id", nullable = false)
+    private User receiver;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ main/src/main/java/io/github/xpakx/images/priv/PrivateMessageRepository.java
@@ -0,0 +1,6 @@
+package io.github.xpakx.images.priv;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrivateMessageRepository extends JpaRepository<PrivateMessage, Long> {
+}
