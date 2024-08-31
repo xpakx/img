acb330d04ae194ce09c08387f6ed185955a3a295
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-31 12:15:41 +0200

    feat(main): process SpEL expression inside custom cache aspect

--- main/src/main/java/io/github/xpakx/images/cache/CacheAspect.java
+++ main/src/main/java/io/github/xpakx/images/cache/CacheAspect.java
@@ -4,7 +4,13 @@ import io.github.xpakx.images.cache.annotation.CacheDecrement;
 import io.github.xpakx.images.cache.annotation.CacheIncrement;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.CacheManager;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Service;
 
 import org.aspectj.lang.JoinPoint;
@@ -15,6 +21,7 @@ import org.aspectj.lang.annotation.Pointcut;
 @Service
 @RequiredArgsConstructor
 public class CacheAspect {
+    Logger logger = LoggerFactory.getLogger(CacheAspect.class);
     private final CacheManager cacheManager;
 
     @Pointcut("@annotation(cacheIncrement)")
@@ -28,18 +35,27 @@ public class CacheAspect {
     @AfterReturning(value = "cacheIncrementPointcut(cacheIncrement)", argNames = "joinPoint,cacheIncrement")
     public void incrementAfterReturning(JoinPoint joinPoint, CacheIncrement cacheIncrement) {
         String cacheName = cacheIncrement.value();
-        String keyExpression = cacheIncrement.key();
-        System.out.println("INCREMENT");
-        System.out.println("CACHE NAME:" + cacheName);
-        System.out.println("CACHE KEY:" + keyExpression);
+        String key = parseKey(joinPoint, cacheIncrement.key());
+        logger.debug("Increment {} in cache {}", key, cacheName);
     }
 
     @AfterReturning(value = "cacheDecrementPointcut(cacheDecrement)", argNames = "joinPoint,cacheDecrement")
     public void decrementAfterReturning(JoinPoint joinPoint, CacheDecrement cacheDecrement) {
         String cacheName = cacheDecrement.value();
-        String keyExpression = cacheDecrement.key();
-        System.out.println("DECREMENT");
-        System.out.println("CACHE NAME:" + cacheName);
-        System.out.println("CACHE KEY:" + keyExpression);
+        String key = parseKey(joinPoint, cacheDecrement.key());
+        logger.debug("Decrement {} in cache {}", key, cacheName);
+    }
+
+    private String parseKey(JoinPoint joinPoint, String keyExpression) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        ExpressionParser parser = new SpelExpressionParser();
+        StandardEvaluationContext context = new StandardEvaluationContext();
+
+        var parameterNames = signature.getParameterNames();
+        var args = joinPoint.getArgs();
+        for (int i = 0; i < parameterNames.length; i++) {
+            context.setVariable(parameterNames[i], args[i]);
+        }
+        return parser.parseExpression(keyExpression) .getValue(context, String.class);
     }
 }
