44b3783bf184a271c64705065a3a1020fee9a8fe
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-06 14:34:26 +0200

    chore(cdc): add automatic config for cassandra keyspace/table

---
+++ cdc/embedded/cassandra-config/init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Creating keyspace and table..."
+cqlsh cassandra -u admin -p admin -e "CREATE KEYSPACE IF NOT EXISTS images_cassandra WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
+cqlsh cassandra -u admin -p admin -e "CREATE TABLE IF NOT EXISTS images_cassandra.event (id uuid, name text,  PRIMARY KEY(id));"
