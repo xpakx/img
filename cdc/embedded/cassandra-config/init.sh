2e7d6640e54cd7f91b2da3b29b9894fa8fc0d707
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-07 11:09:03 +0200

    chore(cdc): make sure configuration wait till cassandra is available

--- cdc/embedded/cassandra-config/init.sh
+++ cdc/embedded/cassandra-config/init.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+until printf "" 2>>/dev/null >>/dev/tcp/cassandra/9042; do 
+    sleep 5;
+    echo "Waiting for cassandra...";
+done
 
 echo "Creating keyspace and table..."
 cqlsh cassandra -u admin -p admin -e "CREATE KEYSPACE IF NOT EXISTS images_cassandra WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
