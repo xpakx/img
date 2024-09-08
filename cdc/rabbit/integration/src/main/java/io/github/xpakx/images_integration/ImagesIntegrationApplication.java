707753639d256aa5efb72b52ee4eea8324efeec8
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-08 15:42:26 +0200

    chore(cdc/integration): init integration service

---
+++ cdc/rabbit/integration/src/main/java/io/github/xpakx/images_integration/ImagesIntegrationApplication.java
@@ -0,0 +1,12 @@
+package io.github.xpakx.images_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagesIntegrationApplication.class, args);
+	}
+}
---
+++ cdc/rabbit/integration/src/test/java/io/github/xpakx/images_integration/ImagesIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.images_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
