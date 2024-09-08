74019c6d899d0f4ed1ae7109be01a772020b1d96
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-08 15:43:11 +0200

    chore(cdc/integration): configure amqp

---
+++ cdc/rabbit/integration/src/main/java/io/github/xpakx/images_integration/config/AMQPConfig.java
@@ -0,0 +1,29 @@
+package io.github.xpakx.images_integration.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AMQPConfig {
+    @Bean
+    Exchange changeExchange(@Value("${rabbitmq.exchange}") String changeExchangeName) {
+        return ExchangeBuilder.topicExchange(changeExchangeName)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Queue changeQueue(@Value("${rabbitmq.queue}") String changeQueueName) {
+        return QueueBuilder.durable(changeQueueName)
+                .build();
+    }
+
+    @Bean
+    Binding worksBinding(Queue changeQueue, Exchange changeExchange) {
+        return BindingBuilder
+                .bind(changeQueue)
+                .to(changeExchange).with("change").noargs();
+    }
+}
