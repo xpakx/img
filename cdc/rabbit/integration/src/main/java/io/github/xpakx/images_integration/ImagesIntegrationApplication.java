7a7187856cac30ba4df0aa65c308e04aa9009dcc
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-08 15:43:26 +0200

    feat(cdc/integration): consume CDC messages from rabbitMQ queue

--- cdc/rabbit/integration/src/main/java/io/github/xpakx/images_integration/ImagesIntegrationApplication.java
+++ cdc/rabbit/integration/src/main/java/io/github/xpakx/images_integration/ImagesIntegrationApplication.java
@@ -1,7 +1,13 @@
 package io.github.xpakx.images_integration;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.Transformers;
+import org.springframework.integration.amqp.dsl.Amqp;
 
 @SpringBootApplication
 public class ImagesIntegrationApplication {
@@ -9,4 +15,18 @@ public class ImagesIntegrationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ImagesIntegrationApplication.class, args);
 	}
+
+	@Bean
+	IntegrationFlow handle(ConnectionFactory connectionFactory, @Value("${rabbitmq.queue}") String queueName) {
+		return IntegrationFlow
+				.from(Amqp.inboundAdapter(connectionFactory, queueName))
+				.transform(Transformers.fromJson())
+				.handle((payload, headers) -> {
+					System.out.println(payload.getClass().getName());
+					System.out.println(payload);
+					return null;
+				})
+				.get();
+	}
+
 }
