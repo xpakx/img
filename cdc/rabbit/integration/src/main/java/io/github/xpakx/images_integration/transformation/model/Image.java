a7ffabab92d2867a328956d104a60948e3facd68
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-09 13:41:03 +0200

    feat(cdc/integration): process image events

--- cdc/rabbit/integration/src/main/java/io/github/xpakx/images_integration/transformation/model/Image.java
+++ cdc/rabbit/integration/src/main/java/io/github/xpakx/images_integration/transformation/model/Image.java
@@ -1,4 +1,12 @@
 package io.github.xpakx.images_integration.transformation.model;
 
-public record Image() {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record Image(
+        Long id,
+        @JsonProperty("image_url") String imageUrl,
+        String caption,
+        @JsonProperty("user_id") String userId,
+        @JsonProperty("created_at") String created_at
+) {
 }
