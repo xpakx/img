e3ec7e92d2983d7ff07d248f77fa27f995bdca72
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-09 12:40:22 +0200

    chore: change cdc rabbitmq exchange to topic

--- cdc/rabbit/init.sh
+++ cdc/rabbit/init.sh
@@ -1,3 +1,3 @@
 curl -i -u rabbit:password -H "content-type:application/json" \
-    -XPUT -d'{"type":"fanout","durable":true}' \
+    -XPUT -d'{"type":"topic","durable":true}' \
     http://rabbitmq:15672/api/exchanges/%2f/images.cdc.topic
