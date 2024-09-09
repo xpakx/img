d91c06640883f0dd59bb61bd732749ee4435e474
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-09 13:33:50 +0200

    feat(cdc/integration): parse and transform debezium event

--- cdc/rabbit/integration/src/main/java/io/github/xpakx/images_integration/transformation/model/Account.java
+++ cdc/rabbit/integration/src/main/java/io/github/xpakx/images_integration/transformation/model/Account.java
@@ -1,4 +1,8 @@
 package io.github.xpakx.images_integration.transformation.model;
 
-public record Account() {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties({"password"})
+public record Account(Long id, String username, @JsonProperty("avatar_url") String avatarUrl) {
 }
--- cdc/rabbit/integration/src/main/java/io/github/xpakx/images_integration/transformation/model/Image.java
+++ cdc/rabbit/integration/src/main/java/io/github/xpakx/images_integration/transformation/model/Image.java
@@ -1,4 +1,4 @@
 package io.github.xpakx.images_integration.transformation.model;
 
-public record Event<T>(T before, T after, String tableName) {
+public record Image() {
 }
