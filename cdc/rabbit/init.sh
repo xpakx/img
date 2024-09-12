df44a353949640762f89bacf47b54a6056ea0a32
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-12 11:10:33 +0200

    chore: make sure rabbitmq init container run after rabbitmq is up

--- cdc/rabbit/init.sh
+++ cdc/rabbit/init.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+until curl -u rabbit:password -f -s http://rabbitmq:15672/api/overview >/dev/null; do 
+    sleep 5;
+    echo "Waiting for rabbitMQ management API...";
+done
+
+echo "Creating CDC topic..."
 curl -i -u rabbit:password -H "content-type:application/json" \
     -XPUT -d'{"type":"topic","durable":true}' \
     http://rabbitmq:15672/api/exchanges/%2f/images.cdc.topic
